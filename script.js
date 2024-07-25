bb29bdb869190a88260addbb6a13206371104088
jaoneves24 <joaovcjvnq132@gmail.com>
2024-07-25 13:17:01 -0300

    Update script.js
    melhoria do codigo

--- script.js
+++ script.js
@@ -1,20 +1,60 @@
-const tarefaInput = document.getElementById("tarefa");
-const adicionarBotao = document.getElementById("adicionar");
-const listaTarefas = document.getElementById("tarefas");
+let Ntarefa = document.querySelector('#Ntarefa');
+let add = document.querySelector('#add');
+let lista = document.querySelector('#lista');
 
-adicionarBotao.addEventListener("click", adicionarTarefa);
-tarefaInput.addEventListener("keypress", function(e) {
-  if (e.key === "Enter") {
-    adicionarTarefa();
+Ntarefa.addEventListener('keypress', (e) => {
+
+  if(e.keycode = 15){ 
+    //um "=" para o elemento "keycode" funcionar
+    
+    let tarefa = {
+      nome: Ntarefa.value,
+      id: gerarid()
+    }
+    addtarefa(tarefa);
   }
+
+});
+add.addEventListener('click', (e)=>{
+  let tarefa = {
+    nome: Ntarefa.value,
+    id: gerarid(),
+  }
+  addtarefa(tarefa);
 });
 
-function adicionarTarefa() {
-  const tarefaTexto = tarefaInput.value;
-  if (tarefaTexto.trim() !== "") {
-    const novaTarefa = document.createElement("li");
-    novaTarefa.innerHTML = tarefaTexto;
-    listaTarefas.appendChild(novaTarefa);
-    tarefaInput.value = "";
-  }
+function gerarid() {
+  return Math.floor(Math.random()* 3000);
+}
+
+function addtarefa(tarefa) {
+  let li = cirarLI(tarefa);  
+  lista.appendChild(li);
+  Ntarefa.value = '';
+
+}
+
+function cirarLI(tarefa){
+
+  let li = document.createElement('li');
+  let span= document.createElement('span');
+  span.classList.add('tarefas');
+  span.innerHTML = tarefa.nome;
+
+  let div = document.createElement('div');
+
+  let editar = document.createElement('button');
+  editar.classList.add('editar');
+
+  let excluir = document.createElement('button');
+  excluir.classList.add('excluir');
+  
+
+  div.appendChild(editar);
+  div.appendChild(excluir);
+
+  li.appendChild(span);
+  li.appendChild(div);
+  return li;
+
 }
